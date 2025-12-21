927d9c087facecc57f1ce3e6f3d9421ad4a8df0c
DImarmar <ardhiah11@gmail.com>
2025-12-22 02:20:59 +0900

    Assigment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Hero from "./components/container/HeroSection";
-
+import { Button } from "./components/ui/Button/Button";
 function App() {
   return (
     <>
       <h1 className="text-3xl">code here</h1>
       {/* example */}
       <Hero />
+      <Button>Hello</Button>
+      <br />
+      <Button>Hallo</Button>
     </>
   );
 }
